aa9131be769f7b9c7f9bfb025412aea6755dfadb
Jeronim Morina <jeronim@bloomedai.com>
2026-03-06 22:59:21 +0100

    chore: 🔧 add omaship formula 0.2.1

---
+++ Formula/omaship.rb
@@ -0,0 +1,19 @@
+class Omaship < Formula
+  desc "CLI for provisioning and managing Omaship ships"
+  homepage "https://omaship.com"
+  url "https://rubygems.org/downloads/omaship-0.2.1.gem"
+  sha256 "8b9daf715b27b4af51a4891970b4ae00a337f2e4aced094a8e79592306aa9c40"
+  license "Proprietary"
+
+  depends_on "ruby"
+
+  def install
+    ENV["GEM_HOME"] = libexec
+    system "gem", "install", cached_download, "--no-document", "--install-dir", libexec
+    bin.env_script_all_files(libexec/"bin", GEM_HOME: ENV["GEM_HOME"], GEM_PATH: ENV["GEM_HOME"])
+  end
+
+  test do
+    assert_match "omaship", shell_output("#{bin}/omaship --help")
+  end
+end
